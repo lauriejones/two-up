ae189ba3cb78268acaf779442d638338de128c65
lauriejones <lauriej91@gmail.com>
2020-04-25 14:44:58 +1000

    Styling fixes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
 import React, { useState } from "react";
-import styled, { keyframes, css } from "styled-components";
+import styled from "styled-components";
 import { Helmet } from "react-helmet";
 import Results from "./Results";
-import {
-  HEADS,
-  TAILS,
-  NO_WINNER,
-  INITIAL_COINS,
-  COIN_HEADS,
-  COIN_TAILS
-} from "./constants";
+import Coin from "./Coin";
+import { HEADS, TAILS, NO_WINNER, INITIAL_COINS } from "./constants";
 import "./styles.css";
 
-const fadeIn = keyframes`
-  0% {
-    transform: rotateX(0) scale(0.8);
-  }
-  100% {
-    transform: rotateX(5turn) scale(0.01);
-  }
-`;
-const Coin = styled.div`
-  width: 20vmin;
-  height: 20vmin;
-  border-radius: 999px;
-  background-color: silver;
-  ${props =>
-    props.face === HEADS
-      ? ` background: linear-gradient(45deg, ${COIN_HEADS}, silver);`
-      : ` background: linear-gradient(45deg, ${COIN_TAILS}, silver);`};
-  box-shadow: inset 0 0 0 0.5rem hsla(0, 0%, 0%, 0.2);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  ::before {
-    ${props => (props.face === HEADS ? `content: 'H'` : `content: 'T'`)};
-    font-size: 10vmin;
-    font-weight: bold;
-  }
-
-  transition: all 0.2s;
-  transform: scale(1);
-
-  ${props =>
-    props.loading &&
-    css`
-      animation: 3s ${fadeIn} infinite;
-    `}
-`;
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, minmax(100px, 1fr));
@@ -56,11 +13,12 @@ const Grid = styled.div`
   grid-template-areas:
     "header header"
     "coin1 coin2"
-    "result result"
-    "toss-again toss-again";
+    "toss-again toss-again"
+    "result result";
   gap: 3rem;
   justify-items: center;
   max-width: 800px;
+  width: 100%;
   margin: auto;
 `;
 const Stack = styled.div`
@@ -80,22 +38,30 @@ const Coin2 = styled.div`
 const TossAgain = styled.button`
   grid-area: toss-again;
   padding: 1rem 2rem;
-  background-color: blue;
+  background-color: #3f51b5;
+  background: linear-gradient(to bottom, #2196f3, #3f51b5);
   color: white;
+  border: none;
+  appearance: none;
   border-radius: 1rem;
   font-size: 1.5rem;
   font-weight: bold;
   cursor: pointer;
+  box-shadow: inset 0 0 0 0.0625rem hsla(217, 62%, 16%, 0.1),
+    0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
+  transition: transform 0.2s ease-in-out;
 
-  &:hover {
-    background-color: royalblue;
+  &:hover,
+  &:focus {
+    background-color: linear-gradient(to bottom, #03a9f4, #3f51b5);
+    transform: translateY(-2px);
   }
 `;
 
 const randomResult = () => Math.floor(Math.random() * 2);
 const headsOrTails = () => (randomResult() === 0 ? HEADS : TAILS);
 const tossCoins = () => [headsOrTails(), headsOrTails()];
-const getWinner = result => (result[0] === result[1] ? result[0] : NO_WINNER);
+const getWinner = (result) => (result[0] === result[1] ? result[0] : NO_WINNER);
 
 export default function App() {
   const [coins, setCoins] = useState(INITIAL_COINS);
@@ -112,7 +78,7 @@ export default function App() {
       setCount(count + 1);
       setCoins(result);
       setHistory([...history, getWinner(result)]);
-    }, 3000);
+    }, 2500);
   };
 
   const clearHistory = () => {
@@ -139,16 +105,20 @@ export default function App() {
         <Coin1>
           <Stack>
             <Coin face={coins[0]} loading={loading ? true : undefined} />
-            <p>{loading ? "..." : coins[0]}</p>
+            <p>{loading ? "" : coins[0]}</p>
           </Stack>
         </Coin1>
 
         <Coin2>
           <Stack>
             <Coin face={coins[1]} loading={loading ? true : undefined} />
-            <p>{loading ? "..." : coins[1]}</p>
+            <p>{loading ? "" : coins[1]}</p>
           </Stack>
         </Coin2>
+
+        <TossAgain onClick={tossFunction} disabled={loading ? true : undefined}>
+          Toss 'em{count > 0 && " again"}!
+        </TossAgain>
 
         <Results
           coins={coins}
@@ -157,10 +127,6 @@ export default function App() {
           clearHistory={clearHistory}
           loading={loading}
         />
-
-        <TossAgain onClick={tossFunction} disabled={loading ? true : undefined}>
-          Toss 'em{count > 0 && " again"}!
-        </TossAgain>
       </Grid>
     </div>
   );
---
+++ src/Coin.js
@@ -0,0 +1,135 @@
+import React from "react";
+import styled, { keyframes, css } from "styled-components";
+import { HEADS, TAILS, COIN_HEADS, COIN_TAILS } from "./constants";
+
+const flipAnimation = keyframes`
+  20% {
+    transform: rotateX(1turn) scale(1.25);
+  }
+
+  100% {
+    transform: rotateX(-5turn) scale(0.01);
+  }
+`;
+
+const flipTailsAnimation = keyframes`
+  20% {
+    transform: rotateX(-0.5turn) scale(1.25);
+  }
+
+  100% {
+    transform: rotateX(6turn) scale(0.01);
+  }
+`;
+
+const Card = styled.div`
+  position: relative;
+  width: 20vmin;
+  height: 20vmin;
+  border-radius: 999px;
+  /* perspective: 500px; */
+`;
+
+const Content = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  transition: transform 1s;
+  transform-style: preserve-3d;
+
+  ${(props) =>
+    props.loading &&
+    css`
+      animation: 2.5s linear
+        ${props.face === HEADS ? flipAnimation : flipTailsAnimation} infinite;
+    `}
+
+  ${(props) =>
+    props.face === TAILS &&
+    `
+    transform: rotateX(180deg);
+  `}
+`;
+
+const Front = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background: white;
+  line-height: 300px;
+  color: #03446a;
+  text-align: center;
+  font-size: 60px;
+  border-radius: 5px;
+  backface-visibility: hidden;
+`;
+
+const Back = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background: white;
+  line-height: 300px;
+  color: #03446a;
+  text-align: center;
+  font-size: 60px;
+  border-radius: 5px;
+  backface-visibility: hidden;
+
+  transform: rotateX(180deg);
+`;
+
+const BaseCoin = styled.div`
+  width: 20vmin;
+  height: 20vmin;
+  border-radius: 999px;
+  background-color: silver;
+  box-shadow: inset 0 0 0 0.5rem hsla(0, 0%, 0%, 0.2),
+    inset 1px 1px 2px 0.5rem hsla(0, 0%, 100%, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  text-shadow: 1px 1px 2px rgba(255, 255, 255, 0.8);
+
+  ::before {
+    font-size: 10vmin;
+    font-weight: bold;
+  }
+
+  transition: all 0.2s;
+  transform: scale(1);
+  backface-visibility: hidden;
+`;
+
+const Heads = styled(BaseCoin)`
+  position: absolute;
+  background: linear-gradient(45deg, ${COIN_HEADS}, silver);
+  ::before {
+    content: "H";
+  }
+`;
+
+const Tails = styled(BaseCoin)`
+  position: absolute;
+  background: linear-gradient(45deg, ${COIN_TAILS}, silver);
+  ::before {
+    content: "T";
+  }
+`;
+
+const Coin = (props) => (
+  <Card face={props.face}>
+    <Content loading={props.loading} face={props.face}>
+      <Front>
+        <Heads />
+      </Front>
+      <Back>
+        <Tails />
+      </Back>
+    </Content>
+  </Card>
+);
+
+export default Coin;
